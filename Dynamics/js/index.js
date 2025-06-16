ed8c9ab1fbf9f3db91c5e0634644655773448e8d
DanteVasq <324267381@alumno.enp.unam.mx>
2025-06-16 11:45:19 -0600

    Intento de funcionamiento de agregar en carrito

---
+++ Dynamics/js/index.js
@@ -0,0 +1,18 @@
+//Obtener los elementos por Id
+let carrito=document.getElementById("cart-list")
+let btn1=document.getElementById("Art1");
+let btn2=document.getElementById("Art2");
+let btn3=document.getElementById("Art3");
+let btn4=document.getElementById("Art4");
+let btn5=document.getElementById("Art5");
+let btn6=document.getElementById("Art6");
+let btn7=document.getElementById("Art7");
+let btn8=document.getElementById("Art8");
+let btn9=document.getElementById("Art9");
+
+//Poner valores en nuestro carrito
+btn1.addEventListener("click", ()=>{
+    let art1;
+    art1.document.getElementByClassName("cart-item");
+    carrito.appendChild("art1");
+});
